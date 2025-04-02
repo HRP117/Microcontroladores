a8a521f2fad474eb55264002fa154caf736147bc
HRP117 <72773916+HRP117@users.noreply.github.com>
2025-04-01 22:34:52 -0600

    Ejercicio para encender una fila de 4 LEDs

---
+++ EJERCICIOS/EncenderFoco/EncenderFoco.ino
@@ -0,0 +1,19 @@
+#define LED 9
+
+void setup() {
+  //Definir el pin, salida digital
+  pinMode(LED, OUTPUT);
+
+}
+
+void loop() {
+  //1. Encender el LED
+  digitalWrite(LED, HIGH);
+  //2. Esperar medio segundo
+  delay(2000);
+  //3. Apagar el LED
+  digitalWrite(LED, LOW);
+  //4. Esperar medio segundo
+  delay(4000);
+  
+}
