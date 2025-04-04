535e2f14eecd0b7a72505e7d63eba439ff2d7076
HRP117 <72773916+HRP117@users.noreply.github.com>
2025-04-04 11:51:54 -0600

    Ejercicio para observar el funcionamiento de una interrupcion con contador de segundos que se reseta cuando se aprieta el boton

---
+++ EJERCICIOS/InterrupcionLCDContReset/InterrupcionLCDContReset.ino
@@ -0,0 +1,34 @@
+#include <LiquidCrystal.h>
+const int LED = 13;
+const int BOTON = 2;
+volatile int EdoLED = 0;
+const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
+LiquidCrystal LCD(rs, en, d4, d5, d6, d7);
+int contador = 0;
+
+void setup()
+{
+    pinMode(LED, OUTPUT);
+    pinMode(BOTON, INPUT);
+    attachInterrupt(digitalPinToInterrupt(BOTON), ISRLED, RISING);
+    LCD.begin(16, 2);
+    LCD.setCursor(0, 0);
+    LCD.clear();
+}
+void loop()
+{
+    digitalWrite(LED, EdoLED);
+    delay(10);
+
+    contador++;
+    delay(1000);
+    LCD.clear();
+    // LCD.autoscroll();
+    LCD.print(contador);
+    LCD.print(" segundos");
+}
+void ISRLED()
+{
+    EdoLED = !EdoLED;
+    contador = 0;
+}
